358d5c82c3d8265223a7c10a47d7b871e7d7ebb1
italicshark <alex.akulov07@gmail.com>
2026-09-10 17:21:44 -0400

    Change from 20 to 10 tickets

--- akulov_progex_1.py
+++ akulov_progex_1.py
@@ -13,29 +13,31 @@ def display_result(tickets_remaining):
     print("Tickets remaining: ", tickets_remaining)
 
 # beginning variable
-tickets_remaining = 20
-buyers = 0
+def main():
+    tickets_remaining = 10
+    buyers = 0
 
 # loop until tickets end
-while tickets_remaining > 0:
-    display_result(tickets_remaining)
-    tickets_requested = get_ticket()
+    while tickets_remaining > 0:
+        display_result(tickets_remaining)
+        tickets_requested = get_ticket()
 
     # user may only buy between 1 and 4 tickets
-    if tickets_requested < 1 or tickets_requested > 4:
-        print("You may purchase between 1 and 4 tickets!")
+        if tickets_requested < 1 or tickets_requested > 4:
+            print("You may purchase between 1 and 4 tickets!")
 
     # only 20 tickets no more
-    elif tickets_requested > tickets_remaining:
-        print("There are not enough tickets!")
+        elif tickets_requested > tickets_remaining:
+            print("There are not enough tickets!")
 
     # buyer and ticket counter
-    else:
-        tickets_remaining = tickets_remaining - tickets_requested
-        buyers += 1
+        else:
+            tickets_remaining = tickets_remaining - tickets_requested
+            buyers += 1
 
-        display_result(tickets_remaining)
+            display_result(tickets_remaining)
 
-# print results
-print("All tickets have been sold!")
-print("Total number of buyers:", buyers)
+    # print results
+    print("All tickets have been sold!")
+    print("Total number of buyers:", buyers)
+main()
